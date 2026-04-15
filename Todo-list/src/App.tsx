44faf5ee878506376f608e7a296a6b764d4bfb73
Alan <alanpetris.dev@gmail.com>
2026-04-15 18:25:40 -0300

    Projeto Todo List iniciado

---
+++ Todo-list/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import Header from "./components/Header.tsx";
+
+function App() {
+    return (
+        <>
+            <h1>Hello World</h1>
+            <Header />
+        </>
+    );
+}
+
+export default App;
---
+++ Todo-list/src/components/Header.tsx
@@ -0,0 +1,10 @@
+function Header() {
+    return (
+        <div className="">
+            <h1 className="text-red-500">To-do List</h1>
+            <button>Add</button>
+        </div>
+    );
+}
+
+export default Header;
